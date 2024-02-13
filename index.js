fe533cb47cd6e553aa4c58b5872da1269f38f644
LuiV228 <luqmaan.toffey@gmail.com>
2024-02-13 08:40:36 +0200

    JSON file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello there");
+import express from "express";
+
+// Express app
+const app = express();
+// Router
+const Router = express.Router();
+// Port
+const port = +process.env.port || 4000;
+// JSON URL
+const dataURL = "https://luiv228.github.io/vue_portfolio-lt-json/data/";
+// Application Middleware
+app.use(Router);
+// / => home
+Router.get("^/$|/ejd", (req, res) => {
+  res.json({ status: res.statusCode, msg: "You're home" });
+});
+// /educations
+Router.get("/skills", async (req, res) => {
+  let { skills } = await (await fetch(dataURL)).json();
+  res.json({
+    status: res.statusCode,
+    skills,
+  });
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
